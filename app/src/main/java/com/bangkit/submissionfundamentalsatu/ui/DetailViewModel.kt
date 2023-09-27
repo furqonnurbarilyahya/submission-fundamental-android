76aacf0496f74f6f78fc3090a86716cbef59ec31
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-09-27 17:55:24 +0700

    hapus function get follower dan get following

--- app/src/main/java/com/bangkit/submissionfundamentalsatu/ui/DetailViewModel.kt
+++ app/src/main/java/com/bangkit/submissionfundamentalsatu/ui/DetailViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bangkit.submissionfundamentalsatu.data.response.DetailUserResponse
-import com.bangkit.submissionfundamentalsatu.data.response.ItemsItem
 import com.bangkit.submissionfundamentalsatu.data.retrofit.ApiConfig
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,16 +22,8 @@ class DetailViewModel : ViewModel() {
     private val _detailUser = MutableLiveData<DetailUserResponse> ()
     val detailUser: LiveData<DetailUserResponse> = _detailUser
 
-    private val _followers = MutableLiveData<List<ItemsItem>>()
-    val followers: LiveData<List<ItemsItem>> = _followers
-
-    private val _following = MutableLiveData<List<ItemsItem>>()
-    val following: LiveData<List<ItemsItem>> = _following
-
     init {
         detailUser()
-        getFollowers()
-        getFollowing()
     }
 
     fun detailUser (username: String = "") {
@@ -58,53 +49,4 @@ class DetailViewModel : ViewModel() {
 
         })
     }
-
-    fun getFollowers (username: String = "") {
-        _isLoading.value = true
-        val client = ApiConfig.getApiService().getFollowers(username)
-        client.enqueue(object : Callback<List<ItemsItem>>{
-            override fun onResponse(
-                call: Call<List<ItemsItem>>,
-                response: Response<List<ItemsItem>>
-            ) {
-                _isLoading.value = false
-                if (response.isSuccessful) {
-                    _followers.value = response.body()
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<List<ItemsItem>>, t: Throwable) {
-                _isLoading.value = false
-                Log.e(TAG,  "onFailure: ${t.message}")
-            }
-
-        })
-    }
-
-    fun getFollowing (username: String = "") {
-        _isLoading.value = true
-        val client = ApiConfig.getApiService().getFollowing(username)
-        client.enqueue(object : Callback<List<ItemsItem>>{
-            override fun onResponse(
-                call: Call<List<ItemsItem>>,
-                response: Response<List<ItemsItem>>
-            ) {
-                _isLoading.value = false
-                if (response.isSuccessful) {
-                    _following.value = response.body()
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<List<ItemsItem>>, t: Throwable) {
-                _isLoading.value = false
-                Log.e(TAG,  "onFailure: ${t.message}")
-            }
-
-        })
-    }
-
 }
