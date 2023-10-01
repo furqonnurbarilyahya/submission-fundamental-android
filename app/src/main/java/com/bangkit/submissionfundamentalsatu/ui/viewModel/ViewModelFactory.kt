726b39927cdf88a05d33cde5cd4b16171424351f
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-10-01 17:09:45 +0700

    add class ViewModelFactory

--- app/src/main/java/com/bangkit/submissionfundamentalsatu/ui/viewModel/ViewModelFactory.kt
+++ app/src/main/java/com/bangkit/submissionfundamentalsatu/ui/viewModel/ViewModelFactory.kt
@@ -1,13 +1,19 @@
 package com.bangkit.submissionfundamentalsatu.ui.viewModel
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bangkit.submissionfundamentalsatu.data.source.SettingPreferences
 import com.bangkit.submissionfundamentalsatu.data.source.UserRepository
 import com.bangkit.submissionfundamentalsatu.di.Injection
 
+val Context.dataStore:DataStore<Preferences> by preferencesDataStore(name = "settings")
 class ViewModelFactory private constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val preferences: SettingPreferences
 ): ViewModelProvider.NewInstanceFactory() {
 
     companion object {
@@ -17,7 +23,7 @@ class ViewModelFactory private constructor(
             context: Context
         ): ViewModelFactory =
             INSTANCE ?: synchronized(this) {
-                INSTANCE ?: ViewModelFactory(Injection.provideRepository(context))
+                INSTANCE ?: ViewModelFactory(Injection.provideRepository(context), SettingPreferences.getInstance(context.dataStore))
             }.also { INSTANCE = it }
     }
 
@@ -27,6 +33,8 @@ class ViewModelFactory private constructor(
             return FavoriteUserViewModel(userRepository) as T
         } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
             return DetailViewModel(userRepository) as T
+        } else if (modelClass.isAssignableFrom(SettingViewModel::class.java)) {
+            return SettingViewModel(preferences) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
     }
