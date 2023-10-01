f2e35e3714f95ec379478b0a8a2bd6a661f34d22
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-10-01 11:01:09 +0700

    add FavoriteUserActivity

---
+++ app/src/main/java/com/bangkit/submissionfundamentalsatu/ui/activity/FavoriteUserActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.submissionfundamentalsatu.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.submissionfundamentalsatu.R
+
+class FavoriteUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_user)
+    }
+}
