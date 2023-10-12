808ee488fdda0b5f40368272ce4aad3e18f3b771
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-10-12 20:47:01 +0700

    delete class AppExecutors

--- app/src/main/java/com/bangkit/submissionfundamentalsatu/di/Injection.kt
+++ app/src/main/java/com/bangkit/submissionfundamentalsatu/di/Injection.kt
@@ -3,7 +3,6 @@ package com.bangkit.submissionfundamentalsatu.di
 import android.content.Context
 import com.bangkit.submissionfundamentalsatu.data.source.UserRepository
 import com.bangkit.submissionfundamentalsatu.database.room.FavoriteUserDatabase
-import com.bangkit.submissionfundamentalsatu.util.AppExecutors
 
 object Injection {
     fun provideRepository(context: Context): UserRepository {
